91dd7d0e308c261a9a8c9f66b1fe1edd881b920c
yahyaaboumeil <yahyaaboumeil@gmail.com>
2026-05-15 15:44:16 +0100

    add channel class without mergin

---
+++ channel/channel.cpp
@@ -0,0 +1,269 @@
+#include "channel.hpp"
+
+
+Channel::Channel()
+{
+    _topic = "";
+    _key = "";
+    _inviteOnly = false;
+    _topicRestricted = false;
+    _userLimit = -1;
+}
+
+std::string Channel::getName()
+{
+    return _name;
+}
+
+bool Channel::isEmpty()
+{
+    return (_vClient.empty());
+}
+
+bool Channel::isOperator(Client *client)
+{
+    for (std::vector<Client*>::iterator it = _vOperator.begin(); it != _vOperator.end(); it++)
+    {
+        if ((*it)->socket_fd == client->socket_fd)
+            return true;
+    }
+    return false;
+}
+
+void Channel::addClient(Client *client)
+{
+    _vClient.push_back(client);
+}
+
+void Channel::removeClient(Client *client, bool printMSG)
+{
+    if (isEmpty())
+        return;
+    for (std::vector<Client*>::iterator it = _vClient.begin(); it != _vClient.end(); it++)
+    {
+        if (*it == client)
+        {
+            if (printMSG)
+                broadcast(client->getNickName() + "PART" + _name, client);
+            _vClient.erase(it);
+            break;
+        }
+    }
+}
+
+void Channel::broadcast(std::string message, Client *sender)
+{
+    std::string newMessage = "PRIVMSG #" + sender->getNickName() + _name + " :" + message;
+
+    for (std::vector<Client*>::iterator it = _vClient.begin(); it != _vClient.end(); it++)
+    {
+        if ((*it)->socket_fd != sender->socket_fd)
+            (*it)->send(newMessage);
+    }
+}
+
+void Channel::addOperator(Client *client)
+{
+    _vOperator.push_back(client); 
+}
+
+void Channel::removeOperator(Client *client)
+{
+    for (std::vector<Client*>::iterator it = _vOperator.begin(); it != _vOperator.end(); it++)
+    {
+        if (*it == client)
+        {
+            _vOperator.erase(it);  
+            break;
+        }
+    }
+}
+
+void Channel::setTopic(std::string topic)
+{
+    _topic = topic;
+}
+
+std::string Channel::getTopic()
+{
+    return _topic;
+}
+
+void Channel::setTopicRestricted(bool value)
+{
+    _topicRestricted = value;
+}
+
+bool Channel::isTopicRestricted()
+{
+    return _topicRestricted;
+}
+
+bool Channel::isInviteOnly()
+{
+    return _inviteOnly;
+}
+
+void Channel::setInviteOnly(bool value)
+{
+    _inviteOnly = value;
+}
+
+std::string Channel::getKey()
+{
+    return _key;
+}
+
+void Channel::setKey(std::string key)
+{
+    _key = key;
+}
+
+bool Channel::hasKey()
+{
+    return !_key.empty();
+}
+
+int Channel::getUserLimit()
+{
+    return _userLimit;
+}
+
+void Channel::setUserLimit(int l)
+{
+    _userLimit = l;
+}
+
+bool Channel::hasLimit()
+{
+    return _userLimit != -1;
+}
+
+void handleMode(Channel &channel, std::string mode, std::string arg)
+{
+    if (mode == "+i")
+        channel.setInviteOnly(true);
+    else if (mode == "-i")
+        channel.setInviteOnly(false);
+    else if (mode == "+k")
+        channel.setKey(arg);
+    else if (mode == "-k")
+        channel.setKey("");
+    else if (mode == "+l")
+        channel.setUserLimit(std::atoi(arg.c_str()));
+    else if (mode == "-l")
+        channel.setUserLimit(-1);
+}
+
+bool Channel::canJoin(Client *client, std::string key)
+{
+    if (hasLimit() && _vClient.size() >= (size_t)_userLimit)
+        return false;
+    if (hasKey() && _key != key)
+        return false;
+    if (isInviteOnly() && !isInvited(client))
+        return false;
+    return true;
+}
+
+void Channel::inviteClient(Client *client)
+{
+    _vInvitedClients.push_back(client);
+}
+
+bool Channel::isInvited(Client *client)
+{
+    for (std::vector<Client*>::iterator it = _vInvitedClients.begin(); it != _vInvitedClients.end(); it++)
+    {
+        if (client == *it)
+            return true;
+    }
+    return false;
+}
+
+void Channel::removeInvitation(Client *client)
+{
+    for (std::vector<Client*>::iterator it = _vInvitedClients.begin(); it != _vInvitedClients.end(); it++) 
+    {
+        if (*it == client)
+        {
+            _vInvitedClients.erase(it);
+            return;
+        }
+    }
+}
+
+void Channel::kickClient(Channel &channel, Client *sender, Client *target)
+{
+    if (!channel.isOperator(sender))
+    {
+        sender->send("482 #" + channel.getName() + " :You're not channel operator");
+        return;
+    }
+    channel.broadcast(":" + sender->getNickName() + " KICK #" + channel._name + " " + target->getNickName(), sender);
+    removeClient(target, false);
+    removeOperator(target);
+}
+
+bool Channel::isRestrictedTopic()
+{
+    return _topicRestricted;
+}
+
+void Channel::ChangeTopin(Channel &channel, Client *sender, std::string topic)
+{
+    if (isRestrictedTopic() && !isOperator(sender))
+    {
+        sender->send("482 #" + channel.getName() + " :You're not channel operator");
+    }
+    channel.broadcast(":" + sender->getNickName() + " TOPIC #" + channel._name + " :" + topic,
+                  sender);
+    channel.setTopic(topic); 
+}
+
+void Channel::inviteToChannel(Channel &channel,Client *sender, Client *target)
+{
+    if (!channel.isOperator(sender))
+    {
+        sender->send("482 #" + channel.getName() + " :You're not channel operator");
+        return;
+    }
+    channel.inviteClient(target);
+    target->send(":" + sender->getNickName()+ " INVITE "+ target->getNickName()+ " #" + channel._name); 
+}
+
+void Join(Channel &channel, Client *client)
+{
+    if (channel.isEmpty())
+        channel.addOperator(client);
+}
+
+bool Channel::hasClient(Client *client)
+{
+    for (std::vector<Client*>::iterator it = _vClient.begin(); it != _vClient.end(); it++)
+    {
+        if (*it == client)
+            return true;
+    }
+    return false;
+}
+
+std::vector<std::string> Channel::getClientList()
+{
+    std::vector<std::string> clientList;
+    for (std::vector<Client*>::iterator it = _vClient.begin(); it != _vClient.end(); it++)
+    {
+        clientList.push_back((*it)->getNickName());
+    }
+    return clientList;
+}
+
+bool Channel::hasDuplicateNickName(std::string nickname)
+{
+    for (std::vector<Client*>::iterator it = _vClient.begin(); it != _vClient.end(); it++)
+    {
+        if ((*it)->getNickName() == nickname)
+            return true;
+    }
+    return false;
+}
---
+++ channel/channel.hpp
@@ -0,0 +1,90 @@
+#ifndef CHANNEL_HPP
+#define CHANNEL_HPP
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cstdlib>
+
+
+class Client
+{
+    public:
+        int socket_fd;
+        std::string getNickName();
+        void send(std::string Message);
+};
+
+class Channel
+{
+    private:
+        std::string             _name;
+        std::string             _topic;
+        std::string             _key;
+
+        bool                    _inviteOnly;
+        bool                    _topicRestricted;
+
+        int                     _userLimit;
+
+        std::vector<Client*>    _vClient;
+        std::vector<Client*>    _vOperator;
+        std::vector<Client*>    _vInvitedClients;
+
+    public:
+        Channel();
+
+        // getters
+        std::string getName();
+        std::string getTopic();
+        std::string getKey();
+        int         getUserLimit();
+
+        // setters
+        void setTopic(std::string topic);
+        void setTopicRestricted(bool value);
+        void setInviteOnly(bool value);
+        void setKey(std::string key);
+        void setUserLimit(int l);
+
+        // checks
+        bool isEmpty();
+        bool isOperator(Client *client);
+        bool isTopicRestricted();
+        bool isRestrictedTopic();
+        bool isInviteOnly();
+        bool hasKey();
+        bool hasLimit();
+        bool hasClient(Client *client);
+        bool canJoin(Client *client, std::string key);
+        bool isInvited(Client *client);
+        bool hasDuplicateNickName(std::string nickname);
+
+        // clients
+        void addClient(Client *client);
+        void removeClient(Client *client, bool printMSG);
+        void broadcast(std::string message, Client *sender);
+
+        // operators
+        void addOperator(Client *client);
+        void removeOperator(Client *client);
+
+        // invitations
+        void inviteClient(Client *client);
+        void removeInvitation(Client *client);
+
+        // commands
+        void kickClient(Channel &channel, Client *sender, Client *target);
+        void ChangeTopin(Channel &channel, Client *sender, std::string topic);
+        void inviteToChannel(Channel &channel, Client *sender, Client *target);
+
+        // utils
+        std::vector<std::string> getClientList();
+};
+
+// non-member functions
+void handleMode(Channel &channel, std::string mode, std::string arg);
+void Join(Channel &channel, Client *client);
+
+#endif
+
